0e3fbcddc8c9ea1be9d9db1add203da0de4dcc10
Oravec Erik <oravec@mail.thot-soft.com>
2019-05-20 11:53:00 +0200

    docker updated

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+// localstring: './../frontend/dist/BookShop/'
+
 router.get('*', function (req, res) {
     console.log(req.originalUrl);
     /*    const extOfUrl = req.originalUrl.split('.').pop();*/
 
     const indexOfExt = req.originalUrl.lastIndexOf('.');
     if (indexOfExt === -1) {
-        return res.sendFile(path.join(__dirname + './../frontend/dist/BookShop/index.html'));
+        return res.sendFile(path.join(__dirname + './../bookshop/index.html'));
     }
     const extOfUrl = req.originalUrl.slice(indexOfExt + 1);
 
@@ -18,10 +20,10 @@ router.get('*', function (req, res) {
     for (let extension of extensions) {
         console.log(extension);
         if (extOfUrl === extension) {
-            return res.sendFile(path.join(__dirname + './../frontend/dist/BookShop/' + req.originalUrl));
+            return res.sendFile(path.join(__dirname + './../bookshop/' + req.originalUrl));
         }
     }
-    return res.sendFile(path.join(__dirname + './../frontend/dist/BookShop/index.html'));
+    return res.sendFile(path.join(__dirname + './../bookshop/index.html'));
 });
 
 //add the router
